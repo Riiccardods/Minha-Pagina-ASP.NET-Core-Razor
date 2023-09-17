f51351fa963f00cd6b13bd0dcbba7c9da42abb8f
Riiccardods <riicardoljn@icloud.com>
2023-09-17 19:45:05 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public string Mensagem { get; set; } = "OLA";
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync(string novaMensagem)
+    {
+        if (!string.IsNullOrWhiteSpace(novaMensagem))
+        {
+            Mensagem = novaMensagem;
+        }
+
+        return Page();
+    }
+}
